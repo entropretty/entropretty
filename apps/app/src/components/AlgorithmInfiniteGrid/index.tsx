5112f1d4176c735583a3ef8ea706a0ce7e36f845
peetzweg <phil.czek@gmail.com>
2025-09-02 10:13:13 +0200

    hides demo and autoscroll button on mobile

--- apps/app/src/components/AlgorithmInfiniteGrid/index.tsx
+++ apps/app/src/components/AlgorithmInfiniteGrid/index.tsx
@@ -104,8 +104,8 @@ const AlgorithmActions = ({ algorithm }: { algorithm: AlgorithmView }) => {
 
   return (
     <div className="flex w-auto flex-row items-center justify-end gap-2 md:w-auto">
-      <AutoScrollButton />
-      <Button asChild variant="link">
+      <AutoScrollButton className="hidden md:block" />
+      <Button className="hidden md:block" asChild variant="link">
         <Link to={`/demo/${algorithm.id}`}>{`DEMO`}</Link>
       </Button>
       {user && (
