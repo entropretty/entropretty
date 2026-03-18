c3a09c3d3b65db16296f9fff9230cf43461359d4
peetzweg <phil.czek@gmail.com>
2026-03-18 13:33:11 +0100

    vocs docs setup

---
+++ docs/vocs.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "vocs"
+
+export default defineConfig({
+  title: "Entropretty",
+  description: "Beautiful visualizations of entropy",
+  basePath: "/entropretty",
+  rootDir: ".",
+  sidebar: [
+    {
+      text: "Getting Started",
+      link: "/getting-started",
+    },
+  ],
+})
